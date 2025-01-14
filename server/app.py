4d61599e29629899a77d985e43d907832242cbac
Joseph <darkknight505c@gmail.com>
2025-01-14 12:55:50 +0300

    undone

--- server/app.py
+++ server/app.py
@@ -4,5 +4,32 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/parameter')
+def print_string(string):
+    print(string)
+    return string
+
+@app.route('/count/parameter')
+def count(number):
+    return '\n'.join(str(i) for i in range(number)) + '\n'
+
+@app.route('/math/<num1>/<operation>/<num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        return str(num1 + num2)
+    elif operation == '-':
+        return str(num1 - num2)
+    elif operation == '*':
+        return str(num1 * num2)
+    elif operation == 'dev':
+        return str(num1 / num2)
+    elif operation == '%':
+        return str(num1 % num2)
+    return "invalid operation"
+    
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
